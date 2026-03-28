1f6e99e107f7d0f015181d7716a002c4db92feb4
Кирилл Мансуров <kirill.mansurov.2025@mail.ru>
2026-03-29 01:49:50 +0300

    update

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py migrate --noinput
 python manage.py run_import
 python manage.py collectstatic --noinput
 cp -r /app/collected_static/. /backend_static/static/
-gunicorn --bind 0.0.0.0:8000 backend.wsgigi
+gunicorn --bind 0.0.0.0:8000 backend.wsgi
